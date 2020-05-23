9939e0da314e9ea261a6013e4196251b70a08ceb
Donkey-1028 <anbo10283182@gmail.com>
2020-05-23 20:25:16 +0900

    추천 알고리즘 프로토타입 추가

--- recommend/views.py
+++ recommend/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from order.models import OrderProduct
 
+
+def test(request):
+    content = OrderProduct.objects.filter(product_id=9)
+    order_range = []
+    for index, value in enumerate(content):
+        order_range.append(value.order.id)
+    content2 = OrderProduct.objects.filter(order_id__in=order_range)
+    print(content2)
+    return render(request, 'recommend/test.html', {'content':content2})
 # Create your views here.
