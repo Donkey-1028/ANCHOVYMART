5d23c34dfaa4389b7f3aaea80928954dc2fd3245
Donkey-1028 <anbo10283182@gmail.com>
2020-05-26 01:47:05 +0900

    Rate Model 생성, admin site 등록

--- recommend/models.py
+++ recommend/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.contrib.auth.models import User
+
+from order.models import OrderProduct
+
+class Rate(models.Model):
+    order_product = models.OneToOneField(OrderProduct, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rate= models.PositiveIntegerField(default=1, validators=[MaxValueValidator(5), MinValueValidator(1)])
 
 # Create your models here.
