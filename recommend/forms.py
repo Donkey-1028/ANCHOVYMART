2d2f3dc50862ee6de1f4fc1fda3df367f5013a19
Donkey-1028 <anbo10283182@gmail.com>
2020-05-27 14:03:29 +0900

    rate validate 프로토타입 추가

--- recommend/forms.py
+++ recommend/forms.py
@@ -6,3 +6,10 @@ class RateForm(forms.ModelForm):
     class Meta:
         model = Rate
         fields = ['rate']
+
+    def clean_rate(self):
+        cd = self.cleaned_data['rate']
+        if 1 > cd or cd > 5:
+            print('11111111111111111111111111111111111111111')
+            raise forms.ValidationError('평점 에러')
+        return cd
