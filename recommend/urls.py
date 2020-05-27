bf4180e1271bbefb80e5d0e6441b82827797b0bb
Donkey-1028 <anbo10283182@gmail.com>
2020-05-27 16:38:28 +0900

    전체 추천 상품 기능 추가

--- recommend/urls.py
+++ recommend/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import  InputRate, ShowRecommendProducts, ShowUnratedProducts
+from .views import  InputRate, ShowRecommendProducts, ShowUnratedProducts, NonPersonalProducts
 
 app_name = 'recommend'
 
@@ -7,5 +7,6 @@ urlpatterns = [
     path('input_rate/<int:order_product_pk>', InputRate.as_view(), name='input_rate'),
     path('show_recommend_products/', ShowRecommendProducts.as_view(), name='show_recommend_products'),
     path('show_unrated_products/',ShowUnratedProducts.as_view(), name='show_unrated_products'),
+    path('non_personal_products/', NonPersonalProducts.as_view(), name='non_personal_products'),
 
 ]
--- recommend/views.py
+++ recommend/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render, redirect
 from django.views.generic.base import View
 from django.views.generic.list import ListView
 from django.contrib import messages
+from django.core.paginator import Paginator
 
 from order.models import OrderProduct
+
+from shop.models import Product
 
 from .forms import RateForm
 
@@ -13,6 +16,7 @@ class InputRate(View):
     def get(self, request, order_product_pk):
         form = RateForm()
         order_product = OrderProduct.objects.get(id=order_product_pk)
+        messages.warning(request, '평점의 범위를 1부터 5까지로 부여해주세요')
         return render(request, 'recommend/input_rate.html', {'form':form, 'order_product':order_product})
 
     def post(self, request, order_product_pk):
@@ -23,15 +27,18 @@ class InputRate(View):
             if form.is_valid():
                 form.instance.user = self.request.user
                 form.instance.order_product = order_product
-                form.save()
                 order_product.rated = True
+                order_product.product.rated += form.instance.rate
+                order_product.product.rate_count += 1
+                order_product.product.save()
                 order_product.save()
+                form.save()
+            else:
+                form = RateForm()
+                return redirect('recommend:input_rate', order_product_pk)
 
         return redirect('recommend:show_recommend_products')
-"""
-상품들마다 rate 측정할수 있게 페이지 만들기,
-이미 평점이 등록된건 수정할 수도 있게 만들기.
-"""
+
 
 class ShowRecommendProducts(View):
 
@@ -39,6 +46,8 @@ class ShowRecommendProducts(View):
         if OrderProduct.objects.filter(user_id=self.request.user.id, rated=False):
             messages.warning(request, '평점을 부여하지 않은 상품들이 있습니다. 평점을 입력해주세요.')
             return redirect('recommend:show_unrated_products')
+        elif OrderProduct.objects.filter(user_id=self.request.user.id):
+            print('알고리즘처리')
         else:
             print('all')
         return redirect('/')
@@ -55,5 +64,11 @@ class ShowUnratedProducts(ListView):
         context['count'] = len(OrderProduct.objects.filter(user_id=self.request.user.id, rated=False))
         return context
 
+class NonPersonalProducts(ListView):
+    model = Product
+    template_name = 'recommend/non_personal_products.html'
+    paginate_by = 3
 
+    def get_queryset(self):
+        return Product.objects.all().order_by('-rate')[:6]
 
